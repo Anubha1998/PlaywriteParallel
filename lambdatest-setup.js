2bf4f08b0454030ae16625f2b6c954c485b2a38c
Anubha1998 <121358222+Anubha1998@users.noreply.github.com>
2023-10-05 17:48:53 +0530

    Update lambdatest-setup.js

--- lambdatest-setup.js
+++ lambdatest-setup.js
@@ -1,3 +1,407 @@
+
+Inserted suggestion Looking forward to hearing from youConversation opened. 50 messages. All messages read.
+
+Skip to content
+Using LambdaTest Inc. Mail with screen readers
+fevtutor playwright parallel 
+
+1 of 6
+LambdaTest <> Fevtutor <> Trial & Next Steps!
+External
+Inbox
+
+Himanshu Sahu
+Thu, Jun 15, 1:32 PM
+Hey team, Thanks for your time on the call today. I'm adding @Anubha Singh to this email thread to assist you in case you've any queries while connecting your C
+28
+
+anubhas
+Tue, Jul 4, 5:52 PM
+Hi Dharamraj, Thank you for your time over the call. Our dev team is checking for the sample, we'll update you once this is done.
+
+Dharamraj S Afzalpurkar <dharamraj.sa@fevtutor.com>
+Attachments
+Wed, Jul 5, 3:39 PM
+to me, Himanshu
+
+HI Anubhas,
+
+I am able to fix the cross browser issue , please find attached sample code which will work fine.
+
+image.png
+
+image.png
+
+Thanks,
+Dharamraj
+
+QA Manager | Products & Technologies | Focus Edumatics
+
+Mobile: 9108010400 / 8050074442
+
+Email: Dharamraj.sa@fevtutor.com
+
+
+ One attachment
+  •  Scanned by Gmail
+
+Himanshu Sahu <himanshusahu@lambdatest.com>
+Wed, Jul 5, 4:33 PM
+to Dharamraj, me
+
+Hey Dharam,
+
+Glad to know that you've been able to fix the scripts. I guess, there are no open issues at our end for now. Your trial is activated till Friday. 
+
+Please let me know when we can connect to discuss the next steps?
+
+Awaiting your response.
+
+Regards,
+Himanshu
+
+
+Himanshu Sahu <himanshusahu@lambdatest.com>
+Attachments
+Thu, Jul 6, 1:43 PM
+to Pachamuthu, Dharamraj, me
+
+Hey Dharam,
+
+Please find the pricing proposal attached to this email for 1 parallel of web automation and 3 parallels of manual testing on real devices.
+
+@Anubha Singh will share the MOM with you in a while.
+
+Let me know if you have any questions.
+
+Regards,
+Himanshu
+
+ One attachment
+  •  Scanned by Gmail
+
+Anubha Singh <anubhas@lambdatest.com>
+Thu, Jul 6, 4:32 PM
+to Jeethendra, Dharamraj, Pachamuthu, Himanshu
+
+Hi Team, 
+
+Thank you for your time over the call. 
+
+I am sharing MOM for the call with you, please share the requested details with us. 
+
+Meeting date	06/07/2023	Meeting time	11:30 AM - 12:40 PM	Meeting location	Google Meet	Meeting Link	meet.google.com/meu-bfeu-woy
+Meeting called by	pachamuthu.s@fevtutor.com,dharamraj.sa@fevtutor.com
+Facilitator	Anubha Singh
+Attendees	pachamuthu.s@fevtutor.com,dharamraj.sa@fevtutor.com
+Agenda/Issue	1. Arrow pointer missing
+2. Not able to select the student from the check box.
+3. Close button from the popup is not closing immediately, after multiple clicks it is working
+4. Parallel execution is happening sequentially
+Resolution	1.As our dev team has discussed over the call, please provide us the site URL and test credentials to test the issue
+2. @Dharamraj S Afzalpurkar  , we have checked with our dev team and found that the parallel execution is happening at our end via your script however it not working at your end so, can you share your availability for the call so that i can add my dev team there to check the issue, 
+
+Do let us know if you face any issues. 
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Thu, Jul 6, 5:56 PM
+to Dharamraj, Himanshu, Jeethendra, Pachamuthu
+
+Hi Dharamraj, 
+
+Thank you for your time over the call. 
+
+Your parallel execution issue has been resolved now, please let us know if you face any issues.
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Fri, Jul 7, 10:30 AM
+to Pachamuthu, Himanshu, Jeethendra, Dharamraj
+
+Hi Pachamuthu, 
+
+Hope you are doing good. 
+
+Just wanted to follow up on the site URL and test credentials, Kindly share the details with us so that our team can check the issues and resolve them as soon as possible. 
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Wed, Jul 12, 2:38 PM
+to Pachamuthu, Himanshu, Jeethendra, Dharamraj
+
+Hi Pachamuthu, 
+
+Hope you are doing good. 
+
+Please be informed that our dev team is working on the fix of your issues, the expected ETA for the fix release would be 24th July. 
+
+Please let us know if you are facing any other issues, we'll help you out. 
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Pachamuthu S <pachamuthu.s@fevtutor.com>
+Wed, Jul 12, 2:40 PM
+to me, Himanshu, Jeethendra, Dharamraj
+
+Thanks  for the update.
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Thu, Jul 13, 1:02 PM
+to Dharamraj, Himanshu, Jeethendra, Pachamuthu
+
+Hi Dharamraj, 
+
+Hope you are doing good. 
+
+Just wanted to check for the next steps with you, please let us know when can we connect over the call.
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Thu, Jul 13, 1:17 PM
+to Pachamuthu, Himanshu, Jeethendra, Dharamraj
+
+Hi Pachamuthu, 
+
+Hope you are doing good. 
+
+We are trying to login on your portal with tutor account :  (lambdatesttutor1/Passw0rd@123) , it is saying incorrect password, can you please share the correct password with us.
+
+Looking forward to hearing from you.
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Dharamraj S Afzalpurkar <dharamraj.sa@fevtutor.com>
+Thu, Jul 13, 2:12 PM
+to Bob, me, Himanshu, Pachamuthu
+
+Adding in Bob , He is Senior QA manager based out of the US. He is in India this week, I will look at his schedule and arrange a call. 
+
+We are good to go on a yearly plan, we will discuss further on the call.
+
+Thanks,
+Dharamraj
+
+QA Manager | Products & Technologies | Focus Edumatics
+
+Mobile: 9108010400 / 8050074442
+
+Email: Dharamraj.sa@fevtutor.com
+
+
+
+Dharamraj S Afzalpurkar <dharamraj.sa@fevtutor.com>
+Thu, Jul 13, 2:22 PM
+to me, Himanshu, Pachamuthu, Bob
+
+Hi Anubha,
+
+Will  tomorrow 12 pm work for you?
+
+
+Thanks,
+Dharamraj
+
+QA Manager | Products & Technologies | Focus Edumatics
+
+Mobile: 9108010400 / 8050074442
+
+Email: Dharamraj.sa@fevtutor.com
+
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Thu, Jul 13, 2:25 PM
+to Dharamraj, Himanshu, Pachamuthu, Bob
+
+Hi Dharamraj, 
+
+Sure, 12 PM would work. 
+
+I am scheduling the call for 12 PM IST tomorrow, please join the call from here : meet.google.com/crx-rbsv-nwr
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Dharamraj S Afzalpurkar <dharamraj.sa@fevtutor.com>
+Fri, Jul 14, 11:40 AM
+to me, Himanshu, Pachamuthu, Bob
+
+Hi Anubha,
+
+Request you to move the meeting to 12:30 please , we are in a meeting that may get extended.
+
+Thanks,
+Dharamraj
+
+QA Manager | Products & Technologies | Focus Edumatics
+
+Mobile: 9108010400 / 8050074442
+
+Email: Dharamraj.sa@fevtutor.com
+
+
+
+Himanshu Sahu <himanshusahu@lambdatest.com>
+Fri, Jul 14, 12:43 PM
+to Dharamraj, Bob, Pachamuthu, me
+
+Hey team,
+
+Thanks for your time on the call today. As discussed, we're willing to offer you a 13 months subscription.
+
+In order to generate the invoice, I'd require the following details:
+
+Customer Information                   
+
+Company Name(Legal):    
+
+Business Contact::
+
+Billing Contact:    
+
+Business Email:
+
+Billing Email id:   
+
+Shipping Address:
+
+ 
+
+ 
+
+Billing Address:   
+
+Purchase Order Required (Yes/No): No
+
+Billing Telephone:
+
+
+Let me know if you have any questions.
+
+Regards,
+Himanshu
+
+
+Anubha Singh <anubhas@lambdatest.com>
+Mon, Jul 17, 5:14 PM
+to Pachamuthu, Dharamraj, Himanshu
+
+Hi Pachamuthu, 
+
+Hope you are doing good. 
+
+Can you please confirm by testing again that the check box click and close click is working fine or not. 
+
+Regards,
+Anubha Singh
+Solution Engineer - PreSales
++917017810089
+BookMyCalender| LambdaTest
+
+
+Pachamuthu S <pachamuthu.s@fevtutor.com>
+Mon, Jul 17, 6:01 PM
+to me, Dharamraj, Himanshu
+
+Hi Anubha,
+
+Will check and let you know. Thanks for the update.
+
+
+Himanshu Sahu <himanshusahu@lambdatest.com>
+Thu, Jul 20, 4:04 PM
+to Dharamraj, me, Pachamuthu
+
+Hey Dharam,
+
+Hope you're well!
+
+Can you please confirm with your finance team when the invoice would be cleared. I'll update the same to our billing team.
+
+Awaiting your response.
+
+Regards,
+Himanshu
+
+
+Dharamraj S Afzalpurkar <dharamraj.sa@fevtutor.com>
+Thu, Jul 20, 4:05 PM
+to Himanshu, me, Pachamuthu
+
+Sorry my Bad I have not forwarded the invoice to the finance team , will send it over now and get it cleared ASAP.
+
+Thanks,
+Dharamraj
+
+QA Manager | Products & Technologies | Focus Edumatics
+
+Mobile: 9108010400 / 8050074442
+
+Email: Dharamraj.sa@fevtutor.com
+
+
+
+Himanshu Sahu <himanshusahu@lambdatest.com>
+Attachments
+Thu, Jul 20, 4:09 PM
+to Dharamraj, me, Pachamuthu
+
+Hey Dharam,
+
+Sharing the invoice again on this email thread. 
+
+Please find the invoice attached to this email and here is the payment link.
+
+Also, I've got your subscription started already. Please confirm with the finance team when the invoice would be cleared and I'll update the billing team accordingly.
+
+Let me know if you have any questions.
+
+Regards,
+Himanshu
+
+ One attachment
+  •  Scanned by Gmail
+I have paid the invoice.I have transferred the money.Paid.
 import json
 import os
 import urllib
@@ -16,7 +420,7 @@ capabilities = [
             'platform': 'Windows 10',
             'build': 'Playwright With Parallel Build',
             'name': 'Playwright Sample Test on Windows 10 - Firefox',
-            'user': 'dharamraj.sa',
+            'user': 'anubhas',
             'accessKey': 'Kg9gzgVYHGEzdJB521dCBIit9tOASggL3KhMSMm27fBhIvxcpN',
             'network': True,
             'video': True,
@@ -30,7 +434,7 @@ capabilities = [
             'platform': 'MacOS Ventura',
             'build': 'Playwright With Parallel Build',
             'name': 'Playwright Sample Test on Windows 8 - MicrosoftEdge',
-            'user': 'dharamraj.sa',
+            'user': 'anubhas',
             'accessKey': 'Kg9gzgVYHGEzdJB521dCBIit9tOASggL3KhMSMm27fBhIvxcpN',
             'network': True,
             'video': True,
@@ -44,7 +448,7 @@ capabilities = [
             'platform': 'Windows 10',
             'build': 'Playwright With Parallel Build',
             'name': 'Playwright Sample Test on Windows10 - Chrome',
-            'user': 'dharamraj.sa',
+            'user': 'anubhas',
             'accessKey': 'Kg9gzgVYHGEzdJB521dCBIit9tOASggL3KhMSMm27fBhIvxcpN',
             'network': True,
             'video': True,
@@ -58,7 +462,7 @@ capabilities = [
             'platform': 'MacOS Big sur',
             'build': 'Playwright With Parallel Build',
             'name': 'Playwright Sample Test on MacOS Big sur - Chrome',
-            'user': 'dharamraj.sa',
+            'user': 'anubhas',
             'accessKey': 'Kg9gzgVYHGEzdJB521dCBIit9tOASggL3KhMSMm27fBhIvxcpN',
             'network': True,
             'video': True,
@@ -122,3 +526,5 @@ def run_tests_in_parallel():
 # Entry point to run the tests
 if __name__ == '__main__':
     run_tests_in_parallel()
+test_multibrowsertest.py
+Displaying test_multibrowsertest.py.
